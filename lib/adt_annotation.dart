fa503ce7ba77a665e3467e2860c1ed81250ca9d0
Vsevolod <vr.19860507@gmail.com>
2020-06-28 18:09:22 +0300

    test release

---
+++ lib/adt_annotation.dart
@@ -0,0 +1,53 @@
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class ADT {
+  const ADT();
+}
+
+class _Data implements ADT {
+  const _Data();
+}
+
+const data = _Data();
+
+class _Union implements ADT {
+  const _Union();
+}
+
+const union = _Union();
+
+@immutable
+class _Nullable {
+  const _Nullable();
+}
+
+const nullable = _Nullable();
+
+@immutable
+class _Mutable {
+  const _Mutable();
+}
+
+const mutable = _Mutable();
+
+@immutable
+class _Primary {
+  const _Primary();
+}
+
+const primary = _Primary();
+
+@immutable
+class Default {
+  final Object value;
+
+  const Default(this.value);
+}
+
+@immutable
+class _Unused {
+  const _Unused();
+}
+
+const unused = _Unused();
